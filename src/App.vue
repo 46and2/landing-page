c8f96cd77fcf0a0fe751b47ba0c7b595e44e6866
Ilmir Akhmetgareev <iakhmetgareev@wsoft.ru>
2019-10-27 07:46:31 +0500

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <top-nav></top-nav>
+    <image-slider title="Коттедж «Уют» из дерева"></image-slider>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import TopNav from './components/TopNav.vue';
+import Slider from './components/Slider.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    'top-nav': TopNav,
+    'image-slider': Slider,
   },
 };
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  font-family: "MuseoSansCyrl", sans-serif;
 }
 </style>
